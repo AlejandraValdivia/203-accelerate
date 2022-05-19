924198d77099179b137267276e8d4176888b03de
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2022-05-18 17:06:13 -0700

    "Adjusted twitter widget heading size and adjusted padding in the newsletter sign-up button"

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -59,7 +59,7 @@ get_header(); ?>
 			<?php query_posts('posts_per_page=1'); ?>
 			<!-- The loop -->
 			<?php while ( have_posts() ) : the_post(); ?>
-					<h3><?php the_title(); ?></h3>
+					<h2><?php the_title(); ?></h2>
 					<?php the_excerpt(); ?>
   		<?php endwhile; // end of the loop. ?> 
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
