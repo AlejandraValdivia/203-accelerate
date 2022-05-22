24c40018a55d74305e0ddbdc0276db513f23be08
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2022-05-21 18:15:32 -0700

    Added paragraph to contact us button

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the About page
+ * The template for displaying the about page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,10 +14,10 @@
 
 get_header(); ?>
 
-	<div id="primary" class="home-page hero-content">
-			<div class="main-content-about" role="main">
-					
-			<?php while ( have_posts() ) : the_post();
+<div id="primary" class="home-page hero-content">
+	<div class="main-content-about" role="main">
+
+        <?php while ( have_posts() ) : the_post();
             $our_services = get_field('our_services');
             $our_services_description = get_field('our_services_description');
             $icon_1 = get_field('icon_1');
@@ -35,67 +35,65 @@ get_header(); ?>
             $size = "medium";
         ?>
 
-          <?php the_content(); ?> 
-					<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+        <?php the_content(); ?>
+        <?php endwhile; // end of the loop. ?>	   
+	</div><!-- .main-content -->
+    </div><!-- #primary -->
+   
+    <div class="our-service">
+        <h4><?php echo $our_services; ?></h4>
+        <p><?php echo $our_services_description; ?></p>
+    </div> 
 
-	<div class="our-services">
-		<h4><?php echo $our_services; ?></h4>
-		<p><?php echo $our_services_description; ?></p>
-	</div>
+    <div id="secondary"> 
 
-	<div id="secondary">
+        <article class="content-strategy">
+            <div class="icon-left">
+                <?php if($icon_1) { echo wp_get_attachment_image( $icon_1, $size ); } ?>
+            </div>
+            <div class="text-right">
+                <h4><?php echo $strategy; ?></h4>
+                <p><?php echo $strategy_description; ?></p>
+            </div>
+        </article>
 
-			<article class="content-strategy">
-				<div class="icon-left">
-						<?php if($icon_1) { echo wp_get_attachment_image($icon_1, $size); } ?>
-				</div>
-				<div class="text-right">
-					<h4><?php echo $strategy; ?></h4>
-					<p><?php echo $strategy_description; ?></p>				
-				</div>
-			</article>
+        <article class="influencer-mapping">
+            <div class="icon-right">
+                <?php if($icon_2) { echo wp_get_attachment_image( $icon_2, $size ); } ?>
+            </div> 
+            <div class="text-left">
+                <h4><?php echo $mapping; ?></h4>
+                <p><?php echo $mapping_description; ?></p>
+            </div>
+        </article>
 
-				
-				<article class="influencer-mapping">
-					<div class="icon-right">
-						<?php if($icon_2) { echo wp_get_attachment_image($icon_2, $size); } ?>
-					</div>
-					<div class="text-left">
-						<h4><?php echo $mapping; ?></h4>
-						<p><?php echo $mapping_description; ?></p>
-					</div>
-				</article>
+        <article class="social-media-strategy">
+            <div class="icon-left">
+                <?php if($icon_3) { echo wp_get_attachment_image( $icon_3, $size ); } ?>
+            </div>
+            <div class="text-right">
+                <h4><?php echo $social_media_strategy; ?></h4>
+                <p><?php echo $social_media_strategy_description; ?></p>
+            </div>
+        </article>
 
-				<article class="social-media-strategy">
-				<div class="icon-left">
-						<?php if($icon_3) { echo wp_get_attachment_image($icon_3, $size); } ?>
-				</div>
-				<div class="text-right">
-					<h4><?php echo $social_media_strategy; ?></h4>
-					<p><?php echo $social_media_strategy_description; ?></p>				
-				</div>
-				</article>
+        <article class="design-development">
+            <div class="icon-right">
+                <?php if($icon_4) { echo wp_get_attachment_image( $icon_4, $size ); } ?>
+            </div> 
+            <div class="text-left">     
+                <h4><?php echo $design_development; ?></h4>
+                <p><?php echo $design_development_description; ?></p>
+            </div>  
+            </article>
 
-				<article class="design-development">
-					<div class="icon-right">
-						<?php if($icon_4) { echo wp_get_attachment_image($icon_4, $size); } ?>
-					</div>
-					<div class="text-left">
-						<h4><?php echo $design_development; ?></h4>
-						<p><?php echo $design_development_description; ?></p>
-					</div>
-			</article>
-	
-
-	</div><!--#id=secondary ends --->
+        </div>
+    </div>
 
 <nav id="navigation" class="container">
-		<p><?php echo $contact_tagline; ?></p>
-		<p><a class="button" href="<?php echo site_url('/contact/') ?>">Contact Us</a></p>
+    <p>Interested in working with us?</p>
+    <p><a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a></p>
 </nav>
 <br>
-
 
 <?php get_footer(); ?>
