a2dc1284008699fa52f87871d9185fe1a78b3323
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2022-05-23 15:28:15 -0700

    updated front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,76 +13,58 @@
  */
 
 get_header(); ?>
-
-	
-	<div id="primary" class="home-page hero-content"> 
+	<div id="primary" class="home-page hero-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="homepage-hero">
-					<?php the_content(); ?>
-					<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
-				</div>
-				
+				<?php the_content(); ?>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
-	</div><!-- .main-content -->
-</div><!-- #primary -->
-
-<section class="featured-work">
-	<div class="site-content">
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+	<section class="featured-work">
+		<div class="site-content">
 			<h4>Featured Work</h4>
-
-			<ul class="homepage-featured-work">
-					<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-					<!-- The loop -->
+			<div class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 					<?php while ( have_posts() ) : the_post(); 
-							$image_1 = get_field('image_1');
-							$size = "medium";
+						$image_1 = get_field('image_1');
+						$size = "medium";
 					?>
-					
-					<li class="individual-featured-work>
+						<div class="individual-featured-work">
 							<figure>
 								<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image( $image_1, $size ); ?></a>
 							</figure>
 							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					</li>
-			
+						</div>
+					<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</section>
 
-					<?php endwhile; // end of the loop. ?>
-					<?php wp_reset_query(); // resets the altered query back to the original ?>
-			</ul>
-	</div><!--.site-content  -->
-</section>
-
-
-<!----- Recent BLOG post and TWITTER widget  ---->
-
-<!-- RECENT BLOG POST -->
-<section class="recent-posts">
+	<section class="recent-posts">
 		<div class="site-content">
 			<div class="blog-post">
-			<h4>From the Blog</h4>
-			<?php query_posts('posts_per_page=1'); ?>
-			<!-- The loop -->
-			<?php while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_excerpt(); ?>
-  		<?php endwhile; // end of the loop. ?> 
-			<?php wp_reset_query(); // resets the altered query back to the original ?>
- 			</div>
-
-
-			 <!-- Tweet widget starts here -->
-			 <div class="tweet-widget">
-					<h4>Recent Tweet</h4>
-						<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-								<div id="secondary" class="widget-area" role="complementary">
-									<?php dynamic_sidebar( 'sidebar-2' ); ?>
-								</div>
-							<?php endif; ?>
-					<p class="tweet-follow">
-								<a href="https://twitter.com/Alex_V1000" target="_blank">Follow us ›</a>
-					</p>
-			 </div>
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h2><?php the_title(); ?></h2>
+						<?php the_excerpt(); ?>
+					<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+		
+			<div class="tweet-widget">	
+				<h4>Recent Tweet</h4>
+				<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+					<div id="secondary" class="widget-area" role="complementary">
+						<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					</div>
+				<?php endif; ?>
+				<p class="tweet-follow"> 
+					<a href="https://twitter.com/Alex_V1000" target="_blank">Follow Us ›</a>
+				</p>
+			</div>
 		</div>
 	</section>
 
