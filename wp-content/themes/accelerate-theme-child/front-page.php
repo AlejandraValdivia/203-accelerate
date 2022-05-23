90ed09bba8b7f95965ebc785fe569fd1d9394aae
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2022-05-22 21:19:25 -0700

    Updated latest post section

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -56,39 +56,35 @@ get_header(); ?>
 
 <!----- Recent BLOG post and TWITTER widget  ---->
 
-	<div class="blog-twitter-section"><!--.blog-twitter-section starts here--->
+	<section class="recent-posts"><!--.recent-posts starts here--->
 		<div class="site-content">
-				<!--<section class="recent-posts">-->
-							
-							<div class="blog-post">
+					<div class="blog-post">
 								<h4>From the Blog</h4>
 								<?php query_posts('posts_per_page=1'); ?>
 								<!-- The loop -->
-								<?php while ( have_posts() ) : the_post(); ?>
-										<h3><?php the_title(); ?></h3>
-										<?php the_excerpt(); ?>
-								<?php endwhile; // end of the loop. ?> 
+										<?php while ( have_posts() ) : the_post(); ?>
+												<h2><?php the_title(); ?></h2>
+												<?php the_excerpt(); ?>
+										<?php endwhile; // end of the loop. ?> 
 								<?php wp_reset_query(); // resets the altered query back to the original ?>
-							</div>
-		</div><!-- .site-content class ends here -->
-				<!-- </section> -->
+					</div>
+		
+			
 
 								<!-- Tweet widget starts here -->
-				<!--<section class="twitter-posts">-->
-
-								<div class="tweet-widget">
-										<h4>Recent Tweet</h4>
-											<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-													<div id="secondary" class="widget-area" role="complementary">
-													</div>
-															<?php dynamic_sidebar( 'sidebar-2' ); ?>
-													</div>
-													<div class="follow-us-link">	
-															<h4><a href="https://twitter.com/Alex_V1000" target="_blank">Follow us ›</a></h4>
-													</div>
-											<?php endif; ?>
-									</div>
-				<!--	</section> -->
-	</div><!-- Blog Twitter section ends here -->
+				
+					<div class="tweet-widget">
+							<h4>Recent Tweet</h4>
+								<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+										<div id="secondary" class="widget-area" role="complementary">
+													<?php dynamic_sidebar( 'sidebar-2' ); ?>
+										</div>		
+								<?php endif; ?>
+								<p class="follow-us-link">	
+										<h4><a href="https://twitter.com/Alex_V1000" target="_blank">Follow us ›</a></h4>
+								</p>
+					</div>
+				</div><!-- .site-content class ends here -->
+	</section><!-- Blog Twitter section ends here -->
 
 <?php get_footer(); ?>
