1848344d1c4b3b21463cdf22dcee416e778fe2d5
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2022-05-23 17:31:37 -0700

    Updated mobile styles front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,59 +13,68 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="home-page hero-content">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
-	<section class="featured-work">
-		<div class="site-content">
-			<h4>Featured Work</h4>
-			<div class="homepage-featured-work">
-				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
-					<?php while ( have_posts() ) : the_post(); 
-						$image_1 = get_field('image_1');
-						$size = "medium";
-					?>
-						<div class="individual-featured-work">
-							<figure>
-								<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image( $image_1, $size ); ?></a>
-							</figure>
-							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-						</div>
-					<?php endwhile; ?>
-				<?php wp_reset_query(); ?>
-			</div>
-		</div>
-	</section>
 
+<div id="primary" class="home-page hero-content">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+			<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
+
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+
+		<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<figure>
+					<a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image($image_1, $size); ?></a>
+				</figure>
+	
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
+
+			<?php endwhile; ?> 
+			<?php wp_reset_query(); ?>
+		</ul>
+	</div>
+</section>
+
+<div class="stack">
 	<section class="recent-posts">
-		<div class="site-content">
-			<div class="blog-post">
-				<h4>From the Blog</h4>
-				<?php query_posts('posts_per_page=1'); ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-						<h2><?php the_title(); ?></h2>
-						<?php the_excerpt(); ?>
-					<?php endwhile; ?>
-				<?php wp_reset_query(); ?>
-			</div>
-		
-			<div class="tweet-widget">	
-				<h4>Recent Tweet</h4>
-				<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-					<div id="secondary" class="widget-area" role="complementary">
-						<?php dynamic_sidebar( 'sidebar-2' ); ?>
-					</div>
-				<?php endif; ?>
-				<p class="tweet-follow"> 
-					<a href="https://twitter.com/Alex_V1000" target="_blank">Follow Us ›</a>
-				</p>
-			</div>
-		</div>
+	<div class="site-content">
+	<div class="blog-post">
+		<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+			<?php the_excerpt(); ?> 
+			<?php endwhile; ?> 
+		<?php wp_reset_query(); ?>
+	</div>
+	</div>
 	</section>
 
+	<section class="twitter-posts">
+	<div class="twitter-content">
+	<h5>Recent Tweet</h5>
+		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+			<div id="secondary" class="widget-area" role="complementary">
+			</div>
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>				
+			</div>				
+			<div class="read-more-link">
+				<h4><a href="https://twitter.com/Alex_V1000">Follow Us ></a></h4>			
+			</div>
+		<?php endif; ?>
+	</div>
+	</section>
+</div>
 <?php get_footer(); ?>
